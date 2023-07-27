f1c799849634af7aa07caf84ba6d799db94147ff
Trung H. Tran <tranhoangtrung.nob@gmail.com>
2023-07-28 03:13:11 +0900

    add horizontal text

--- pages/index.vue
+++ pages/index.vue
@@ -20,10 +20,11 @@ const canhColors = [
 
 const selectedColor = ref(colors[0])
 const selectedCanhColor = ref(canhColors[0])
+const ten = ref('phungan')
 
 </script>
 <template>
-    <UContainer>
+    <UContainer class="w-96">
         <div class="mt-5">
             <div>
               <h3 class="text-sm font-medium text-gray-900">Màu Dây Hoa</h3>
@@ -72,15 +73,41 @@ const selectedCanhColor = ref(canhColors[0])
                   </RadioGroupOption>
                 </div>
               </RadioGroup>
-            </div>
+            </Logo
         </div> -->
+        <div class="mt-5">
+            <div>
+              <h3 class="text-sm font-medium text-gray-900">Chữ hoặc Logo</h3>
+              <div>
+                <label for="first-name" class="block text-sm font-semibold leading-6 text-gray-900">First name</label>
+                <div class="mt-2.5">
+                    <input v-model="ten" type="text" name="first-name" id="first-name" autocomplete="given-name" class="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+                </div>
+            </div>
 
+            <div class="flex justify-center w-full">
+                <label for="dropzone-file" class="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600">
+                    <div class="flex flex-col items-center justify-center pt-5 pb-6">
+                        <svg class="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 16">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"/>
+                        </svg>
+                        <p class="mb-2 text-sm text-gray-500 dark:text-gray-400"><span class="font-semibold">Click to upload</span> or drag and drop</p>
+                        <p class="text-xs text-gray-500 dark:text-gray-400">SVG, PNG, JPG or GIF (MAX. 800x400px)</p>
+                    </div>
+                    <input id="dropzone-file" type="file" class="hidden" />
+                </label>
+            </div> 
+            </div>
+        </div>
         <div class="mx-auto mt-6 max-w-2xl sm:px-6 lg:grid lg:max-w-7xl lg:grid-cols-3 lg:gap-x-8 lg:px-8">
-            <div class="aspect-h-5 aspect-w-4 lg:aspect-h-4 lg:aspect-w-3 sm:overflow-hidden sm:rounded-lg">
+            <div class="flex justify-center aspect-h-5 aspect-w-4 lg:aspect-h-4 lg:aspect-w-3 sm:overflow-hidden sm:rounded-lg">
                 <img :src="selectedColor.image" alt="Model wearing plain white basic tee."
                     class="h-full w-full object-cover object-center">
                 <img :src="selectedCanhColor.image" alt="Model wearing plain white basic tee."
                     class="absolute h-full w-full object-cover object-center">
+                <div class="justify-self-center relative -rotate-90">
+                    <div class="absolute left-0">{{ ten }}</div>
+                </div>
             </div>
         </div>
     </UContainer>
