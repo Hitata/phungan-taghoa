f31feff46ab982aab0433ab410b36195b703c87b
Trung H. Tran <tranhoangtrung.nob@gmail.com>
2023-07-28 02:43:50 +0900

    adding select color logic

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,8 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: [
-    '@nuxthq/ui'
+    // '@nuxt/image',
+    '@nuxthq/ui',
   ],
   colorMode: {
     preference: 'light',
